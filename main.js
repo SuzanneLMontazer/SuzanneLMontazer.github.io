e80e1683dc7fc173f166436da38017370edeadb1
Suzanne Montazer <s.montazer@comcast.net>
2015-05-17 14:21:46 -0400

    index.html

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 var section1 = document.getElementById("section1");
 var button1 = document.getElementById("button1");
 
-var section2 = document.getElementById("section2);
- var button2 = document.getElementById(button2");
+var section2 = document.getElementById("section2");
+var button2 = document.getElementById("button2");
                                        
                                     
+var showSection1 = function() {
+    section1.style.display = "block";
+    section2.style.display = "none";
+};
+var showSection2 = function() {
+    section1.style.display = "none";
+    section2.style.diplay = "block";
+};
+button1.addEventListener("click", function(e){
+    showSections2();
+});
+button2.addEventListener("click", function(e) {
+  showSection1()
+});
+
+showSection1();
